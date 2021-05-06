246f3e10cc48ae112f0b5ea8369f5a9af448b693
Gabriel Rodrigues <52265978+Apenasgabs@users.noreply.github.com>
2021-05-05 22:13:28 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+int  n, soma, soma2,i;
+;
+
+printf("Forneca um valor: ");
+scanf("%d", &n);
+
+soma = 0;
+soma2 = 0;
+
+ for(i=1; i <= n; i++){
+    if (i <= n){
+      (soma2 = i + i-1);
+         printf("\nResultado: %d / %d\n", i, soma2);
+
+    }
+    }
+return 0;
+}
